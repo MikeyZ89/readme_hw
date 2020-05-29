94b872a7ec597251ae99934f10b3b3cbef41b305
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-05-28 17:54:46 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+  # readme_hw
+  ## Description
+  create a readme page using node.js
+
+  ![GitHub repo size](https://img.shields.io/github/repo-size/MikeyZ89/readme_hw)
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test](#tests)
+  * [Questions](#questions) 
+
+  ## Installation
+  build individual sections that prompt the user for information to then be formatted in standard readme 
+
+  ## Usage
+  open terminal inside of index.js file, use command node index.js and press enter answer the prompted questions
+
+  ## License
+  MIT 
+
+  ![GitHub](https://img.shields.io/github/license/MikeyZ89/readme_hw)
+
+  ## Contributing
+  mike
+
+  ## Tests
+
+  ## Questions
+  * readme
+  * readme
+
+    
---
+++ index.js
@@ -0,0 +1,111 @@
+//Node Modules
+const fs = require("fs");
+const util = require("util");
+
+//NPM Packages
+const inquirer = require("inquirer");
+
+const path = require("path");
+
+function writeFile(filename, data) {
+  return fs.writeFileSync(path.join(process.cwd(), filename), data);
+}
+
+inquirer
+  .prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "What is the title of your project?",
+    },
+    {
+      name: "description",
+      type: "input",
+      message: "What is the description of the project?",
+    },
+    {
+      name: "installation",
+      type: "input",
+      message: "What are the steps required to install your project?",
+    },
+    {
+      name: "license",
+      type: "input",
+      message: "What is your license?",
+    },
+    {
+      name: "contributing",
+      type: "input",
+      message: "Who are the contributors on this project?",
+    },
+    {
+      name: "github",
+      type: "input",
+      message: "What is your GitHub username?",
+    },
+    {
+      name: "repoName",
+      type: "input",
+      message: "What is your GitHub repo name?",
+    },
+    {
+      name: "usage",
+      type: "input",
+      message: "Provide instructions for use of your project",
+    },
+    {
+      name: "tests",
+      type: "input",
+      message: "Insert Examples of tests",
+    },
+    {
+      name: "question1",
+      type: "input",
+      message: "What is your GitHub proflie picture?",
+    },
+    {
+      name: "question2",
+      type: "input",
+      message: "What is your GitHub email??",
+    },
+  ])
+  .then(function (user) {
+    console.log(user);
+    const template = `
+  # ${user.title}
+  ## Description
+  ${user.description}
+
+  ![GitHub repo size](https://img.shields.io/github/repo-size/${user.github}/${user.repoName})
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test](#tests)
+  * [Questions](#questions) 
+
+  ## Installation
+  ${user.installation}
+
+  ## Usage
+  ${user.usage}
+
+  ## License
+  ${user.license} 
+
+  ![GitHub](https://img.shields.io/github/license/${user.github}/${user.repoName})
+
+  ## Contributing
+  ${user.contributing}
+
+  ## Tests
+
+  ## Questions
+  * ${user.question1}
+  * ${user.question2}
+
+    `;
+    writeFile("README.md", template);
+});
