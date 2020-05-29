1f6358f434ee71ce632810ba53bb5b2c30111da5
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-05-29 10:06:21 -0700

    edits

--- README.md
+++ README.md
@@ -1,44 +1,45 @@
 
-  # readme generator
+  # readme_hw
 
   ![GitHub last commit](https://img.shields.io/github/last-commit/MikeyZ89/readme_hw) 
-  ![GitHub commit activity](https://img.shields.io/github/commit-activity/y/MikeyZ89/readme_hw) ![GitHub followers](https://img.shields.io/github/followers/MikeyZ89) 
+  ![GitHub commit activity](https://img.shields.io/github/commit-activity/y/MikeyZ89/readme_hw) 
+  ![GitHub followers](https://img.shields.io/github/followers/MikeyZ89) 
   ![GitHub repo size](https://img.shields.io/github/repo-size/MikeyZ89/readme_hw) 
   ![GitHub](https://img.shields.io/github/license/MikeyZ89/readme_hw)
 
   ## Contents:
   * [Authors](#authors)
-  * [Description](#description)
   * [Contributing](#contributing)
-  * [License](#license)
+  * [Acknowledgments](#acknowledgments)
+  * [Description](#description)
   * [Installation](#installation)
   * [Usage](#usage)
   * [Tests](#tests)
   * [Questions](#questions) 
 
   ## Authors:
-  mz
+  Mike Zlotkowski
+
+  ## Contributing:
+  and Company
+
+  ## Acknowledgments:
+  undefined
 
   ## Description:
   readme generator
 
-  ## Contributing:
-  na
-
-  ## License:
-  MIT 
-
   ## Installation:
-  na
+  node.js modules, npm packages, command
 
   ## Usage:
-  na
+  intiate command "node index.js" follow prompted questions about your product
 
   ## Tests:
   na
 
   ## Questions:
-  * na
-  * michael.zlotkowski@gmail.com
+  * Contact Me: michael.zlotkowski@gmail.com
+  * Comments: easy peasy
 
     
--- index.js
+++ index.js
@@ -24,19 +24,9 @@ inquirer
       message: "What is your GitHub repo name?",
     },
     {
-      name: "title",
-      type: "input",
-      message: "Title of the project?",
-    },
-    {
       name: "authors",
       type: "input",
       message: "Who was the Author on the project?",
-    },
-    {
-      name: "description",
-      type: "input",
-      message: "Description of the project?",
     },
     {
       name: "contributing",
@@ -44,9 +34,14 @@ inquirer
       message: "Who were contributors on the project?",
     },
     {
-      name: "license",
+      name: "acknowledgements",
       type: "input",
-      message: "License on the project?",
+      message: "Acknowledgments or inspiration on the project?",
+    },
+    {
+      name: "description",
+      type: "input",
+      message: "Description of the project?",
     },
     {
       name: "installation",
@@ -66,29 +61,30 @@ inquirer
     {
       name: "question1",
       type: "input",
-      message: "Personal comments on the product?",
+      message: "What is your GitHub email??",
     },
     {
       name: "question2",
       type: "input",
-      message: "What is your GitHub email??",
+      message: "Comments?",
     },
   ])
   .then(function (user) {
     console.log(user);
     const template = `
-  # ${user.title}
+  # ${user.repoName}
 
   ![GitHub last commit](https://img.shields.io/github/last-commit/${user.github}/${user.repoName}) 
-  ![GitHub commit activity](https://img.shields.io/github/commit-activity/y/${user.github}/${user.repoName}) ![GitHub followers](https://img.shields.io/github/followers/${user.github}) 
+  ![GitHub commit activity](https://img.shields.io/github/commit-activity/y/${user.github}/${user.repoName}) 
+  ![GitHub followers](https://img.shields.io/github/followers/${user.github}) 
   ![GitHub repo size](https://img.shields.io/github/repo-size/${user.github}/${user.repoName}) 
   ![GitHub](https://img.shields.io/github/license/${user.github}/${user.repoName})
 
   ## Contents:
   * [Authors](#authors)
-  * [Description](#description)
   * [Contributing](#contributing)
-  * [License](#license)
+  * [Acknowledgments](#acknowledgments)
+  * [Description](#description)
   * [Installation](#installation)
   * [Usage](#usage)
   * [Tests](#tests)
@@ -97,14 +93,14 @@ inquirer
   ## Authors:
   ${user.authors}
 
-  ## Description:
-  ${user.description}
-
   ## Contributing:
   ${user.contributing}
 
-  ## License:
-  ${user.license} 
+  ## Acknowledgments:
+  ${user.acknowledgments}
+
+  ## Description:
+  ${user.description}
 
   ## Installation:
   ${user.installation}
@@ -116,8 +112,8 @@ inquirer
   ${user.tests}
 
   ## Questions:
-  * ${user.question1}
-  * ${user.question2}
+  * Contact Me @: ${user.question1}
+  * Comments: ${user.question2}
 
     `;
     writeFile("README.md", template);
